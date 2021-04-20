d22d7f4a27c15d2b632a1e58ad042fd85224b6f2
Darius Brown <Brown.darius.r@gmail.com>
2021-04-20 10:03:34 -0500

    switching

--- js/movies.js
+++ js/movies.js
@@ -3,3 +3,11 @@
 const movieApiURL = "https://peaceful-forest-loaf.glitch.me/movies.";
 
 console.log(movieApiURL);
+
+// $.ajax("/some-url", {
+//     type: "GET",
+//     data: {
+//         name:     "John",
+//         location: "Boston"
+//     }
+// });
