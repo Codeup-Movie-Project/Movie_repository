3d77818d8bf947ad896a9d980ed95cc5c7b409e4
Darius Brown <Brown.darius.r@gmail.com>
2021-04-21 10:22:19 -0500

    user input auto post completed

--- js/movies.js
+++ js/movies.js
@@ -1,32 +1,40 @@
 "use strict";
+function update () {
+    const moviesAjax = $.ajax("https://flower-quiver-giraffatitan.glitch.me/movies");
 
-const moviesAjax = $.ajax("https://peaceful-forest-loaf.glitch.me/movies");
+    function onSuccess(data) {
+        console.log(data);
+    }
 
- function onSuccess(data) {
-     console.log(data);
- }
- function onFail(jqXhr) {
-     console.log("Check your file path.");
- }
- function onAlways() {
-     $('#main').html("Loading...");
- }
-moviesAjax.done(onSuccess);
-moviesAjax.done(onAlways)
+    function onFail(jqXhr) {
+        console.log("Check your file path.");
+    }
 
-moviesAjax.done(function(movies) {
+    function onAlways() {
+        $('#main').html("Loading...");
+    }
 
-    movies.forEach(function (movie) {
-        var content = "";
-        if (movie.title || movie.rating !== undefined) {
-            content += "<h1>" + movie.title + "</h1>"
-            content += "<h1>" + movie.rating + "</h1>"
-            console.log(content);
-            $('#main').append(content);
-        }
+    moviesAjax.done(onSuccess);
+//moviesAjax.done(onAlways);
+//moviesAjax.done(onFail);
+
+    moviesAjax.done(function (movies) {
+        $("#main").html("");
+
+        movies.forEach(function (movie) {
+            var content = "";
+            if (movie.title && movie.rating !== undefined) {
+                content += "<h1>" + movie.title + "</h1>"
+                content += "<h1>" + movie.rating + "</h1>"
+                content += "<button class='delete'>" + movie.id + "</button>"
+                console.log(content);
+                $('#main').append(content);
+            }
+        });
     });
-});
+}
 
+update();
 //let users post movie titles
 
     // const moviesApiURL = "https://peaceful-forest-loaf.glitch.me/movies";
@@ -42,35 +50,78 @@ moviesAjax.done(function(movies) {
     //         'Content-type': 'application/json',
     //     },
     //     body: JSON.stringify(newPost),
-    // }
+    // }https://flower-quiver-giraffatitan.glitch.me/
 
 
-const moviesApiURL = "https://peaceful-forest-loaf.glitch.me/movies";
+    const moviesApiURL = "https://flower-quiver-giraffatitan.glitch.me/movies";
     let movieTitle = document.querySelector('#mtitle');
     let movieRating = document.querySelector('#mrating');
-        let inputtedMovie = movieTitle.value;
-            console.log(inputtedMovie);
-        let inputtedRating = movieRating.value;
-            console.log(inputtedRating);
+    let inputtedMovie = movieTitle.value;
+    console.log(inputtedMovie);
+    let inputtedRating = movieRating.value;
+    console.log(inputtedRating);
 
-        let newPost = {
-            "title": inputtedMovie,
-            "rating": inputtedRating,
-        }
-        let options = {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(newPost),
-        }
-        fetch(moviesApiURL, options).then(function (response) {
-            console.log(response)
-        })
+    let newPost = {
+        "title": inputtedMovie,
+        "rating": inputtedRating,
+    }
+    let options = {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(newPost),
+    }
 
-
-
+     //fetch(moviesApiURL, options).then(function (response) {
+     //   console.log(response)
+  //  })
 
 
 
+//need to replace this with something that posts directly to server
+var ele = document.getElementById('submit');
+ele.addEventListener('click', function(e) {
+    e.preventDefault();
 
+    let inputtedMovie = movieTitle.value;
+    console.log(inputtedMovie);
+    let inputtedRating = movieRating.value;
+    console.log(inputtedRating);
+
+    let newPost = {
+        "title": inputtedMovie,
+        "rating": inputtedRating,
+    }
+    let options = {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(newPost),
+    }
+    console.log(options);
+    fetch(moviesApiURL, options).then(function (response) {
+        update();
+        return response })
+
+});
+
+
+//edit
+
+
+//delete button
+// function deleted() {
+//     let deleteMethod = {
+//         method: "DELETE"
+//     }
+//     fetch(moviesApiURL + "/" +"i", deleteMethod).then(function (response) {
+//         console.log(response);
+//     })
+// };
+// var dele = document.getElementsByClassName('delete');
+// dele.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log("clicked");
+// });
